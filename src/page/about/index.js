691eb2cd2c57982c27d9b0c71d3b72a611db3498
Sophie <2486752510@qq.com>
2018-05-11 20:46:29 +0800

    beforeonline

---
+++ src/page/about/index.js
@@ -0,0 +1,9 @@
+/*
+* @Author: sophie
+* @Date:   2018-05-10 20:58:12
+* @Last Modified by:   sophie
+* @Last Modified time: 2018-05-10 20:59:15
+*/
+'use strict'
+require('page/common/header/index.js');
+require('page/common/nav/index.js');
